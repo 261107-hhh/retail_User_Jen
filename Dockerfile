04ea3c5ff557aa52a677ba3864a66fe5bcdbf40c
Himanshu Nainwal <himanshu.nainwal@STLERI.COM>
2023-06-20 15:25:16 +0530

    docker

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:8
+EXPOSE 9001
+ADD target/retail-user-docker.jar retail-user-docker.jar 
+ENTRYPOINT ["java", "-jar","/retail-user-docker.jar"]
--- src/main/java/com/example/demo/security/JwtHelper.java
+++ src/main/java/com/example/demo/security/JwtHelper.java
@@ -13,7 +13,7 @@ import java.util.function.Function;
 public class JwtHelper {
 
 
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 30 * 60;
 
 
     private String secret = "jwtTokenKey";
